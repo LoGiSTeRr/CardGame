91eb813edf9b961a2272c6e2d7ac047ec75f21d2
LoGiSTeRr <danikbor160@gmail.com>
2022-01-25 15:40:37 +0400

    Add project files.

---
+++ CardGame/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace CardGame
+{
+    public static class Helper
+    {
+        public static bool IsNameValid(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+            {
+                throw new System.ArgumentException("Invalid name");
+            }
+            return true;
+        }
+    }
+    public class Card
+    {
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (Helper.IsNameValid(value))
+                {
+                    name = value;
+                }
+            }
+        }
+
+        private int maxHp;
+        public int MaxHp
+        {
+            get => maxHp;
+            set
+            {
+                if (value > 0)
+                {
+                    maxHp = value;
+                }
+            }
+        }
+
+        private int hp;
+        public int Hp
+        {
+            get => hp;
+            set
+            {
+                if (value <= 0)
+                {
+                    hp = 0;
+                }
+                else if (value >= maxHp)
+                {
+                    hp = maxHp;
+                }
+                else
+                {
+                    hp = value;
+                }
+            }
+        }
+
+        private int atk;
+        public int Atk
+        {
+            get => atk;
+            set
+            {
+                if (value > 0)
+                {
+                    atk = value;
+                }
+            }
+        }
+
+        private int def;
+        public int Def
+        {
+            get => def;
+            set
+            {
+                if (value > 0)
+                {
+                    def = value;
+                }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
